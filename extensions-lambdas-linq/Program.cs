636c3fc4eac2d86a4119956384e15e69b465b7c7
Denis Zavershinskiy <zaverden@gmail.com>
2019-04-07 11:31:48 +0700

    Add project files.

---
+++ extensions-lambdas-linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace extensions_lambdas_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
